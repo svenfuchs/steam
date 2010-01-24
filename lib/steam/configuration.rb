142a8a625506c0ded943a3ce2a6269152cd7d37b
Clemens Kofler <clemens@railway.at>
2010-01-24 17:12:02 +0100

    Make sure we get default config values.

--- lib/steam/configuration.rb
+++ lib/steam/configuration.rb
@@ -5,12 +5,11 @@ module Steam
   end
 
   def self.configure(&block)
-    raise "#configure must be sent a block" unless block_given?
+    raise '#configure must be sent a block' unless block_given?
     yield config
   end
 
   class Configuration
-
     attr_accessor :java_load_params
     attr_accessor :server_name
     attr_accessor :server_port
@@ -20,8 +19,14 @@ module Steam
     attr_accessor :request_env_for
 
     def initialize
+      # always set defaults so we don't depend on init.rb
+      self.java_load_params = '-Xmx1024M'
+      self.server_name      = 'localhost'
+      self.server_port      = '3000'
+      self.rack_url_scheme  = 'http'
+      self.charset          = 'utf-8'
+      self.drb_uri          = 'druby://127.0.0.1:9000'
+      self.request_env_for  = 'http://localhost'
     end
-
   end
-
 end
