4b94baea54742a7d580a45491b1f9b461f7557b4
Sven Fuchs <svenfuchs@artweb-design.de>
2010-02-13 13:13:27 +0100

    make dragdrop test output more clear

--- test/dragdrop_test.rb
+++ test/dragdrop_test.rb
@@ -10,11 +10,12 @@ static = Connection::Static.new(:root => root, :urls => %w(/ /javascripts /style
 @browser = Browser::HtmlUnit.new(Rack::Cascade.new([static, @app]))
 
 def drag_to(target)
+  @browser.page.executeJavaScript('$("#log").text("")')
+
   drag = @browser.locate_in_browser(:div, :class => 'drag')
   drop = @browser.locate_in_browser(:div, :id => target)
 
-  puts "\nDRAGGING (#{drag.getCanonicalXPath}):\n" + drag.asXml
-  puts "DROPPING ONTO (#{drop.getCanonicalXPath}):\n" + drop.asXml
+  puts "\nDROPPING ONTO (#{drop.getCanonicalXPath}):\n" + drop.asXml
 
   drag.mouseDown
   drop.mouseMove
@@ -25,26 +26,32 @@ def drag_to(target)
 end
 
 @browser.request('/index.html')
+drag_to('drop_1')
 drag_to('drop_2')
 drag_to('drop_3')
+drag_to('drop_4')
+drag_to('drop_5')
+drag_to('drop_6')
 
 # OUTPUT:
 #
-# DRAGGING (/html/body/div[4]):
-# <div class="drag ui-draggable" style="position: relative;">
-# </div>
 # DROPPING ONTO (/html/body/div[2]):
 # <div class="drop ui-droppable" id="drop_2">
 # </div>
 # RECEIVED DROP EVENT ON:
 # <div id="drop_1" class="drop ui-droppable"></div>
 #
-# DRAGGING (/html/body/div[4]):
-# <div class="drag ui-draggable" style="position: relative; left: 0px; top: -40px;">
-# </div>
 # DROPPING ONTO (/html/body/div[3]):
 # <div class="drop ui-droppable" id="drop_3">
 # </div>
 # RECEIVED DROP EVENT ON:
-# <div id="drop_1" class="drop ui-droppable"></div><div id="drop_2" class="drop ui-droppable"></div>
+# <div id="drop_2" class="drop ui-droppable"></div>
+#
+# DROPPING ONTO (/html/body/div[4]):
+# <div class="drop ui-droppable" id="drop_4">
+# </div>
+# RECEIVED DROP EVENT ON:
+# <div id="drop_3" class="drop ui-droppable"></div>
+
+
 
