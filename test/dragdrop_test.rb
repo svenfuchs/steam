19bc1e33d1d8a91ab00cc064bd9c6b1a72dc0b64
Sven Fuchs <svenfuchs@artweb-design.de>
2010-02-13 12:22:15 +0100

    add output to dragdrop_test

--- test/dragdrop_test.rb
+++ test/dragdrop_test.rb
@@ -15,7 +15,7 @@ def drag_to(target)
 
   puts "\nDRAGGING (#{drag.getCanonicalXPath}):\n" + drag.asXml
   puts "DROPPING ONTO (#{drop.getCanonicalXPath}):\n" + drop.asXml
-  
+
   drag.mouseDown
   drop.mouseMove
   page = drop.mouseUp
@@ -27,3 +27,24 @@ end
 @browser.request('/index.html')
 drag_to('drop_2')
 drag_to('drop_3')
+
+# OUTPUT:
+#
+# DRAGGING (/html/body/div[4]):
+# <div class="drag ui-draggable" style="position: relative;">
+# </div>
+# DROPPING ONTO (/html/body/div[2]):
+# <div class="drop ui-droppable" id="drop_2">
+# </div>
+# RECEIVED DROP EVENT ON:
+# <div id="drop_1" class="drop ui-droppable"></div>
+#
+# DRAGGING (/html/body/div[4]):
+# <div class="drag ui-draggable" style="position: relative; left: 0px; top: -40px;">
+# </div>
+# DROPPING ONTO (/html/body/div[3]):
+# <div class="drop ui-droppable" id="drop_3">
+# </div>
+# RECEIVED DROP EVENT ON:
+# <div id="drop_1" class="drop ui-droppable"></div><div id="drop_2" class="drop ui-droppable"></div>
+
