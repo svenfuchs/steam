4c3791ffebbd535f62daca988b3274b56ccce5bf
Sven Fuchs <svenfuchs@artweb-design.de>
2010-02-13 19:38:25 +0100

    add an api level test case for HtmlUnit#locate and #within

---
+++ test/browser/html_unit_test.rb
@@ -0,0 +1,77 @@
+require File.expand_path('../../test_helper', __FILE__)
+require 'fixtures/html_fakes'
+
+class HtmlUnitTest < Test::Unit::TestCase
+  include Steam, HtmlFakes
+
+  def setup
+    @app = Steam::Connection::Mock.new
+    static = Steam::Connection::Static.new(:root => FIXTURES_PATH)
+    @browser = Steam::Browser::HtmlUnit.new(Rack::Cascade.new([static, @app]))
+
+    perform(:get, 'http://localhost:3000/', '<div id="foo"><div id="bar"><a id="buz" href="">bar!</a></div></div>')
+  end
+  
+  def locate(*args, &block)
+    @browser.locate(*args, &block)
+  end
+  
+  def within(*args, &block)
+    @browser.within(*args, &block)
+  end
+  
+  test 'locate with node type' do
+    element = locate(:a)
+    assert_equal 'a', element.name
+  end
+  
+  test 'locate with attributes' do
+    element = locate(:id => 'buz')
+    assert_equal 'a', element.name
+  end
+  
+  test 'locate with search text' do
+    element = locate('bar!')
+    assert_equal 'a', element.name
+  end
+  
+  test 'locate with xpath' do
+    element = locate(:xpath => '//div/div/a')
+    assert_equal 'a', element.name
+  end
+  
+  test 'locate with css' do
+    element = locate(:css => '#foo #bar a')
+    assert_equal 'a', element.name
+  end
+  
+  test 'within with node type' do
+    element = within(:div) { within(:div) { locate(:a) } }
+    assert_equal 'a', element.name
+  end
+  
+  test 'within with attributes' do
+    element = within(:id => 'foo') { within(:id => 'bar') { locate(:a) } }
+    assert_equal 'a', element.name
+  end
+  
+  test 'within with xpath' do
+    element = within('//div/div') { locate(:a) }
+    assert_equal 'a', element.name
+  end
+  
+  test 'within with css' do
+    element = within('#foo #bar') { locate(:a) }
+    assert_equal 'a', element.name
+  end
+
+  test 'nesting locate' do
+    element = locate(:id => 'foo') { locate(:id => 'bar') { locate(:a) } }
+    assert_equal 'a', element.name
+  end
+
+  test 'nesting within' do
+    element = within(:id => 'foo') { within(:id => 'bar') { locate(:a) } }
+    assert_equal 'a', element.name
+  end
+end
